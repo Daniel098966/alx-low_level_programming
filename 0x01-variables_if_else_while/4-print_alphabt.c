d7f48a38b3710b58706fb30248c8ea393bcd7de2
“Daniel098966” <daniyayerad@gmail.com>
2022-09-14 13:13:58 -0700

    4-print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - omit alphabet
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		if (h != 'e' && c != 'q')
+		{
+			putchar(h);
+		}
+	}
+	putchar('\n');
+	return (0);
+}

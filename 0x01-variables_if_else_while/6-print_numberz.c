f57955d1d77bf5cd4175e3530ba15d4809439848
Daniel098966 <daniyayerad@gmail.com>
2022-12-12 12:01:05 -0800

    print number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+*main - Entry point
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int c;
+
+	C = 0;
+	While
+		(C < 10) {
+			Putchar(c + ‘0');
+			c++;
+		}
+	Putchar(‘\n’);
+	return (0);
+}
